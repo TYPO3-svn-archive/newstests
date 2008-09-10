2c14b2f44ef0aeda0df6d7c8beabe43819cf1d8a
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-10 20:44:11 +0000

    Add the basic files.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/newstests/trunk@11875 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "newstests"
+#
+# Auto generated 18-08-2008 13:53
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Unit tests for tt_news',
+	'description' => 'This extension contains some unit tests for tt_news for the tutorial at the T3CON08.',
+	'category' => 'example',
+	'author' => 'Oliver Klee',
+	'author_email' => 'typo3-coding@oliverklee.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'test',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:3:{s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"85fc";s:20:"doc/wizard_form.html";s:4:"24a0";}',
+);
+
+?>
