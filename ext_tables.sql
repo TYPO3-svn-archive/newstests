c8b318bc3f80e1229c8ba16087bee3e632b9b81d
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-12 17:13:32 +0000

    Add SQL for is_dummy_record columns for tt_news.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/newstests/trunk@11911 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE tt_news (
+	tx_newstests_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL
+);
+
+CREATE TABLE tt_news_cat (
+	tx_newstests_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL
+);
+
+CREATE TABLE tt_news_related_mm (
+	tx_newstests_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL
+);
+
+CREATE TABLE tt_news_cat_mm (
+	tx_newstests_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL
+);
